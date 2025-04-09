626815bdcc983bc50600e4d0b8eb35783be8a79f
Estarlin Mercedes <estarlinmercedes123@gmail.com>
2025-04-08 22:22:45 -0400

    clase

---
+++ LoDeLosAviones/Filtros.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoDeLosAviones
+{
+
+    public class Filtros
+    {
+        public string Ubicacio;
+        public string Fecha;
+        public string Huespedes;
+        public double Presupuesto;
+
+        
+    } 
+
+
+}
