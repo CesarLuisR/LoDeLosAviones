c22a201e91d138aeebff250e066b3e0ccccb3cea
Cesar Luis Rijo Cedano <cesarluisrc@gmail.com>
2025-04-09 08:23:52 -0400

    Y entonces?

---
+++ LoDeLosAviones/DondeIr.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoDeLosAviones
+{
+    public partial class DondeIr : Form
+    {
+        public DondeIr()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            SuspendLayout();
+            // 
+            // DondeIr
+            // 
+            ClientSize = new Size(282, 253);
+            Name = "DondeIr";
+            Load += DondeIr_Load;
+            ResumeLayout(false);
+        }
+
+        private void DondeIr_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
