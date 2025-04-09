52ebe1db9a015b52fc4d87ce09e04bcb3c75a80e
Cesar Luis Rijo Cedano <cesarluisrc@gmail.com>
2025-04-09 11:26:36 -0400

    Lo de los hoteles dinamicos

---
+++ LoDeLosAviones/HotelInfo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoDeLosAviones
+{
+    [Serializable]
+    public class HotelInfo
+    {
+        public string name;
+        public string description;
+        public string precio;
+        public string ubicacion;
+        public string huespedes;
+        public string imagen;
+
+        public HotelInfo(string name, string description, string precio, string ubicacion, string huespedes, string imagen)
+        {
+            this.name = name;
+            this.description = description;
+            this.precio = precio;
+            this.ubicacion = ubicacion;
+            this.huespedes = huespedes;
+            this.imagen = imagen;
+        }
+
+        static public void Create(string name, string description, string precio, string ubicacion, string huespedes, string imagen)
+        {
+            HotelInfo info = new HotelInfo(name, description, precio, ubicacion, huespedes, imagen);
+            XMLMan.GuardarObjeto(info, Files.hoteles);
+        }
+
+        public HotelInfo() { }
+    }
+}
--- LoDeLosAviones/Program.cs
+++ LoDeLosAviones/Program.cs
@@ -12,6 +12,23 @@ namespace LoDeLosAviones
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
 
+
+            List<HotelInfo> hotelInfos = XMLMan.CargarObjetos<HotelInfo>(Files.hoteles);
+            if (hotelInfos.Count == 0)
+            {
+            HotelInfo.Create("Hotel Paraíso", "Frente a la playa con vista al mar", "150", "Punta Cana", "2 huéspedes", "paraiso.jpg");
+            HotelInfo.Create("Montaña Mágica", "Refugio acogedor en la montaña", "120", "Jarabacoa", "4 huéspedes", "montana.jpg");
+            HotelInfo.Create("Ciudad Colonial", "Hotel boutique en el centro histórico", "180", "Santo Domingo", "2 huéspedes", "colonial.jpg");
+            HotelInfo.Create("Eco Lodge Verde", "Experiencia ecológica en la naturaleza", "100", "Constanza", "3 huéspedes", "eco_verde.jpg");
+            HotelInfo.Create("Resort Bahía Azul", "Resort todo incluido frente al mar", "250", "La Romana", "2 huéspedes", "bahia_azul.jpg");
+            HotelInfo.Create("Villa del Sol", "Villa privada con piscina", "300", "Bávaro", "6 huéspedes", "villa_sol.jpg");
+            HotelInfo.Create("Refugio Tranquilo", "Lugar perfecto para desconectarse", "90", "Barahona", "2 huéspedes", "refugio.jpg");
+            HotelInfo.Create("Hotel Vista Lago", "Vistas impresionantes al lago", "130", "Bonao", "3 huéspedes", "vista_lago.jpg");
+            HotelInfo.Create("Hostal Cultural", "Hospedaje económico con actividades culturales", "60", "Santiago", "1 huésped", "hostal.jpg");
+            HotelInfo.Create("Hotel Aventura", "Ideal para amantes de la aventura", "110", "San Cristóbal", "2 huéspedes", "aventura.jpg");
+
+            }
+
             List<Usuario> usuarios = XMLMan.CargarObjetos<Usuario>(Files.usuarios);
             bool found = false;
             foreach (Usuario user in usuarios)
