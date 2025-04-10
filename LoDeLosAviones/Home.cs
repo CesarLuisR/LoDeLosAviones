48e764b97ec70a34fbb171bd1ea843d66a65d8fa
ALEJANDRO MANUEL RAMIREZ ENCARNACION <ar2023-0608@uce.edu.do>
2025-04-10 15:05:59 -0400

    home

--- LoDeLosAviones/Home.cs
+++ LoDeLosAviones/Home.cs
@@ -332,5 +332,10 @@ namespace LoDeLosAviones
         {
             Filtros.Presupuesto = int.Parse(presupuesto.Value.ToString());
         }
+
+        private void pictureBox3_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
