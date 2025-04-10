ef40c34d0d231b0fbe80672ef1b9503cf1cbb32b
Cesar Luis Rijo Cedano <cesarluisrc@gmail.com>
2025-04-10 18:40:27 -0400

    Terminado

--- LoDeLosAviones/ReservasUsuario.Designer.cs
+++ LoDeLosAviones/ReservasUsuario.Designer.cs
@@ -36,7 +36,7 @@
             // 
             // button1
             // 
-            button1.Location = new Point(483, 29);
+            button1.Location = new Point(956, 29);
             button1.Name = "button1";
             button1.Size = new Size(94, 29);
             button1.TabIndex = 0;
